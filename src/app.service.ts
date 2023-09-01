9a2f77d16fd53b637f0830531b01689c2ba736e6
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-09-02 10:45:14 +1200

    update test suit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Kia Ora This is my first NEST App, HELLO I am so excited! ALso I am struggling';
+    return 'Kia Ora';
   }
 }
